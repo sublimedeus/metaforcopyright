c8bab708e5d13f1266befcce44d41dfce33f2a49
sublimedeus <108197268+sublimedeus@users.noreply.github.com>
2022-06-26 10:06:16 +0300

    Update confirmed.php

--- confirmed.php
+++ confirmed.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 
 
 $username=$_GET['username'];
-$url2="https://dumpor.com/search?query=$username";
+$url2="https://smihub.com/search?query=$username";
 $ip=str_get_html(file_get_contents($url2));
 $pp=$ip->find("img[class='img-fluid w-100']",0)->src;
 $tik="-";
